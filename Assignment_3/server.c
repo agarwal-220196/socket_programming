34a730513718f1d848ef659d3eb73d98d578013d
Sanket Agarwal <agarwal.220196@gmail.com>
2020-10-10 12:05:08 -0700

    RRQ completed

--- Assignment_3/server.c
+++ Assignment_3/server.c
@@ -39,7 +39,13 @@ int check_timeout(int file_descriptor)//timeout mechanisim for checking lost pac
 
 void sigchild_handler()
 {
-
+	int child_errno = errno;
+	int pid;
+	while ((pid=waitpid(-1,NULL,WNOHANG))>0)
+	{
+		printf("child pid: %d \n",pid);
+	}
+	errno = child_errno;
 }
 
 int main(int argc, char *argv[])
@@ -58,12 +64,122 @@ int main(int argc, char *argv[])
 	int child_handle;
 	char sent_data[MAX_DATA_SIZE];
 
-/*following variables will be used afer calling if(!fork()) */
-	char file_name[] = "";
-	FILE *file_pointer;
-	int op_code;
-	int mode_of_op;
-	char temp_file_name[10];
+	if (argc!=3)
+	{
+		printf("Number of arguments are incoorect. PLease use ./server <IP> <portnumber>\n");
+		return 1;
+	}
+
+	memset(&server_address, 0, sizeof(server_address)); //initially resetting every thing to zero. 
+	memset(&client_address,0, sizeof(client_address));
+	memset(receive_buffer,0, sizeof(receive_buffer));
+		
+	server_ip_address = atoi(argv[1]);
+	port_number = atoi(argv[2]);
+
+	//setting the server address now. 
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(port_number);
+
+
+	if ((socket_server = socket(AF_INET,SOCK_DGRAM,0))<0)
+	{
+		printf("server was not created due to %s \n", strerror(errno));
+		return 1;
+	}
+	
+	if ((bind(socket_server, (struct sockaddr*)&server_address, sizeof(server_address)))<0)
+	{
+		printf("Binding process failed due to %s \n",strerror(errno);		
+		close(socket_server);
+		return 1;
+	}
+	
+	printf("Waiting for clients to join...");
+
+	while (1) 
+	{
+		size_of_client = sizeof (client_address);
+		if ((receive_bytes = recvfrom(socket_server, receive_buffer, sizeof(receive_buffer),0,(struct sockaddr*)&client_address, &size_of_client))<0)
+		{
+			printf("unable to receive over socket due to %s \n",strerror(errno));
+			return 1;	
+		}
+		
+		inet_ntop(AF_INET,&client_address.sin_addr, string_max, INET_ADDRSTRLEN);
+		printf("connection estabilished with client from %s \n",string_max);
+
+		sig_add.sa_handler = sigchild_handler;//remove zombie processes.
+		sigemptyset(&sig_add.sa_mask);
+		sig_add.sa_flags = SA_RESTART;
+		
+		if (sigaction(SIGCHLD, &sig_add, NULL)==-1)
+		{	
+			perror("sigaction failure");
+			exit(1);
+		}
+
+		if (!fork())
+		{
+			char file_name[] = "";
+			FILE *file_pointer;
+			int op_code;
+			int mode_of_op;
+			size_t filename_length;
+			char temp_file_name[10];
+			
+			op_code = receive_buffer[1];
+			strcpy(file_name, &receive_buffer[2]);
+			filename_length = strlen (file_name);
+			printf("file name is %s \n",file_name);
+
+// GET the mode of the incoming file. 
+			if (strcasecmp(&receive_buffer[filename_length+3], "netascii")==0)
+				mode_of_op = 1;
+			else if (strcascmp(&receive_buffer[filename_length+3],"octet")==0)
+				mode_of_op = 2;
+			else
+			{
+				//mode not specified. 
+				printf("mode not specified.\n");
+				memset(send_buffer,0, sizeof(send_buffer));
+				send_buffer[1] =5; //error 
+				send_buffer[3] =4; //illegal operation
+
+				stpcpy(&send_buffer[4],"MODE NOT SPECIFIED");
+				filename_length = strlen("MODE NOT SPECIFIED");
+				
+				if (sendto(socket_server, send_buffer, filename_length+5,0,(struct sockaddr *)&client_address, size_of_client)<0)
+				{
+					printf("sending error due to %s\n",strerror(errno));
+					close(socket_server);
+					return 1;
+				}
+	
+			}
+
+			if((file_pointer = fopen(file_name,"r")) ==NULL)
+			{
+				printf("FILE UNAVAILABLE\n");
+				memset(send_buffer,0,sizeof(send_buffer));
+				send_buffer[1] =5;//error
+				send_buffer[3] = 1; //file not found 
+				strcpy(&send_buffer[4],"FILE NOT FOUND");
+				filename_length = strlen("FILE NOT FOUND");
+				if (sendto(socket_server, send_buffer, filename_length+5,0,(struct sockaddr *)&client_address, size_of_client)<0)
+				{
+					printf("sending error due to %s\n",strerror(errno));
+					close(socket_server);
+					return 1;
+				}
+			}
+
+		}
+
+	}
+	
+
 
 }
 
