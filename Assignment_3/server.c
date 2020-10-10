4074fcd90d3fac0117b8450f1ff48d1b92f841ba
dhirajkudva <dhirajkudva@tamu.edu>
2020-10-10 17:20:27 -0500

    rest of the testcode completed

--- Assignment_3/server.c
+++ Assignment_3/server.c
@@ -175,6 +175,222 @@ int main(int argc, char *argv[])
 				}
 			}
 
+			//MODE REQUEST PROCESSING
+			if(mode_of_op == 1)
+			{
+				fseek(file_pointer, 0, SEEK_END);
+				int length_file = ftell(file_pointer);
+				printf("LENGTH OF FILE = %d bytes\n",length_file );
+				fseek(file_pointer, 0, SEEK_SET);
+
+				FILE *temp_file_new;
+				temp_file_new = fopen(temp_file_name, "w");
+
+				int character = 1;
+
+			//UNP REFERENCE ?
+
+				while (character != EOF)
+				{
+					if(next_character >= 0)
+					{
+						fputc(next_character, temp_file_new);
+						next_character = -1;
+						continue;
+					}
+
+					character = fgetc(file_pointer);
+					if(character == EOF)
+					{
+						if(ferror(file_pointer))
+						{
+							printf("ERROR in READ :: fgetc\n");
+							break;
+						}
+					}
+					else if (character == '\n')
+					{
+						character = '\r';
+						next_character = '\n';
+					}
+					else if (character == '\r')
+					{
+						next_character = '\0';
+					}
+					else
+						next_character = -1;
+					fputc(character,temp_file_new);
+
+				}
+				fseek(temp_file_new,0,SEEK_SET);
+				file_pointer = temp_file_new;
+				file_pointer = fopen(temp_file_name,"r");
+
+			}
+			close(socket_server);
+
+			//EPHEMERAL PORT : ANY PORT CAN BE USED BY BOTH SERVER AND CLIENT
+			server_address.sin_port = htons(0);
+
+			//socket creation for child provcess
+			if((child_handle = socket(AF_INET, SOCK_DGRAM,0))<0)
+			{
+				printf("ERROR: CHILD SOCKET cound not be created\n");
+				if(errno)
+					printf("ERROR: EXITING due to : %d\n",strerror(errno) );
+				return 1;
+			}
+
+			//binding socket to listen requests
+			if((bind(child_handle,(struct sockaddr*)&server_address,sizeof(server_address)))<0)
+			{
+				printf("ERROR: UNABLE to bind the socket\n");
+				if(errno)
+					printf("ERROR: exiting due to error :%s\n",strerror(errno) );
+				close(child_handle);
+				return 1;
+			}
+
+			//Set of statements for sending data , ACK, indicating EOF and timeouts
+			//Tracking packets
+
+			unsigned short int number_sentblock, sentnumber;
+			sentnumber=0;
+			number_sentblock = 0;
+
+			unsigned short int final_block, acknowledge_number;
+			final_block = 0;
+			acknowledge_number = 0;
+
+			unsigned short int received_number;
+
+			//track of offset
+			unsigned int offset_file =0;
+			int read_bytes;
+
+			//send
+
+			int sent_handler_bytes, recv_handler_bytes;
+			int count_timeout = 0;
+			int tv;
+
+			while(1)
+			{
+				memset(send_buffer,0, sizeof(send_buffer));
+				sprintf(send_buffer,"%c%c",0x00,0x03);
+
+				sentnumber = number_sentblock + 1 ; //keeping track of the number of blocks and sends
+
+				//writing the second and third entry in the send buffer to indicate this number
+				send_buffer[2]= (sentnumber & 0xFF00) >> 8; //??
+				send_buffer[3] = (sentnumber & 0x00FF);
+
+				fseek(file_pointer,offset_file*MAX_DATA_SIZE, SEEK_SET);
+				memset(sent_data,0,sizeof(sent_data));
+
+				read_bytes = fread(sent_data,1, MAX_DATA_SIZE, file_pointer);
+
+				//checking for the lastblock
+				if(read_bytes < 512)
+				{
+					//indicates that it is the last block
+					if(read_bytes == 0)
+					{
+						send_buffer[4]='\0'; //datablock in buffer
+						printf("BLOCK of zero bytes\n");
+
+					}
+					else
+					{
+						memcpy(&send_buffer[4], sent_data,read_bytes);
+						printf("Last block read: Size of block is less than 512 bytes\n");
+					}
+					final_block = number_sentblock;
+					printf("Final Block = %d\n",final_block );
+				}
+				else
+				{
+					memcpy(&send_buffer[4],sent_data,read_bytes);
+				}
+
+				if((sent_handler_bytes = sendto(child_handle,send_buffer,read_bytes+4,0,(struct sockaddr* )&client_address,size_of_client)) < 0)
+				{
+					printf("ERROR: in sending\n");
+					if(errno)
+						printf("ERROR: Exiting due to error: %s\n",strerror(errno) );
+					break;
+				}
+
+				//checking for timeouts
+				if(tv = check_timeout(child_handle) == 0)
+				{
+					if(count_timeout == 10)
+					{
+						printf("MAXIMUM TIMEOUT REACHED = %d\n",count_timeout );
+						break;
+					}
+					else
+					{
+						printf("ERROR: TIMEOUT OCCURED\n");
+						count_timeout++;
+						continue;
+					}
+				}
+				else if(tv == -1)
+				{
+					printf("ERROR: In child select\n");
+					if(errno)
+						printf("ERROR: Exiting due to error : %s\n",strerror(errno) );
+					break;
+				}
+				else
+				{
+					memset(receive_buffer,0,sizeof(receive_buffer));
+					if((recv_handler_bytes = recvfrom(child_handle,receive_buffer,sizeof(receive_buffer),0,(struct sockaddr*)&client_address,&size_of_client))<0)
+					{
+						printf("ERROR: In Child receive\n");
+						if(errno)
+							printf("ERROR: Exiting error due to :%s\n", strerror(errno));
+						break;
+					}
+
+					op_code = receive_buffer[1];
+					memcpy(&received_number,&receive_buffer[2],2);
+					acknowledge_number = ntohs(received_number);
+
+					printf("OPCODE received from client: %d\n",op_code );
+					printf("Ackowledgement received: %d\n", acknowledge_number );
+
+					if(op_code == 4)
+					{
+						if(acknowledge_number == final_block && read_bytes < 512)
+						{
+							printf("Final Ackowledgement received\n");
+							break;
+						}
+						number_sentblock ++ ;
+						offset_file ++ ;
+
+					}
+					else
+					{
+						printf("Wrong acknowledge number format\n");
+						break;
+					}
+				}
+
+
+			}
+			if(mode_of_op == 1)
+			{
+				if(remove(temp_file_name)!=0)
+					printf("ERROR: In deleting the temp file\n");
+				else
+					printf("Success: Temp file is deleted\n");
+			}
+			close(child_handle);
+			printf("Child is disconnected\n");
+			exit(0);
 		}
 
 	}
