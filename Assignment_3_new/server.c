b0a09b1c7e75b9202e12d68797d86e9562fdeb9d
Sanket Agarwal <agarwal.220196@gmail.com>
2020-10-10 20:25:13 -0700

    adding new struc

---
+++ Assignment_3_new/server.c
@@ -0,0 +1,7 @@
+/*********************************TFTP server implementation*************/
+//Author: Sanket Agarwal and Dhiraj Kudva
+//Organisation: Texas A&M university
+//Usage: Used to handle the TFTP requests from the client end. 
+
+
+
