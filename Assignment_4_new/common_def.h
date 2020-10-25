276d50e1280ca8a4d48b00c9fc347402c884467c
dhirajkudva <dhirajkudva@tamu.edu>
2020-10-25 18:09:53 -0500

    compile correct version of server

--- Assignment_4_new/common_def.h
+++ Assignment_4_new/common_def.h
@@ -38,8 +38,8 @@
 #define TIME_LENGTH 50
 #define FILE_NAME_LENGTH 10
 #define QUEUE 50
-#define FILE 512
-#define BUFFER 10000
+#define FILE_SIZE 512
+#define BUFFER_SIZE 10000
 
 int system_error(const char* error_string);
 // cache definition
@@ -174,14 +174,171 @@ int check_if_cache_entry_present(char *u_r_l)
         }
     }
     return counter;
-	
 }
-
-
-
+int check_if_cache_hit(char *u_r_l){
+    int j=0;
+    for(j=0;j<10;j++){
+        if(strcmp(cache[j].u_r_l, u_r_l) == 0){
+            return 0;
+        }
+    }
+    return -1;
+}
+int connection_accept(struct sockaddr_in *client_address, int count_client, int socket_file_descriptor)
+{
+    socklen_t length =(socklen_t)sizeof(client_address[count_client]);
+    int new_socket_filedescriptor = accept(socket_file_descriptor,(struct sockaddr*)&client_address[count_client], &length);
+    if(new_socket_filedescriptor < 0){
+        system_error("ERROR: IN ACCEPT CONNECTION");
+        exit(-1);
+    }
+    return new_socket_filedescriptor;
+}
+void zombie_handler (int sigtemp_filedescriptor){
+    wait(NULL);
+}
 int system_error(const char* error_string) //display and exit 
 {
 	error(error_string);
 	exit(1);
+}
+
+int month_converter (char *month)
+{
+    int month_number;
+    switch(*month)
+    {
+        case 'J':
+            if(*(month+1) == 'a')
+            {
+                month_number = 1;
+            }
+            else if (*(month+2) == 'n')
+            {
+                month_number = 6;
+            }
+            else
+            {
+                month_number = 7;
+            }
+            break;
+        case 'F':
+            month_number = 2;
+            break;
+        case 'M':
+            if(*(month + 2) == 'r')
+            {
+                month_number = 3;
+            }
+            else
+            {
+                month_number = 5;
+            }
+            break;
+        case 'A':
+            if(*(month+2) == 'r')
+            {
+                month_number = 4;
+            }
+            else
+            {
+                month_number = 8;
+            }
+            break;
+        case 'S':
+            month_number = 9;
+            break;
+        case 'O':
+            month_number = 10;
+            break;
+        case 'N':
+            month_number = 11;
+            break;
+        case 'D':
+            month_number = 12;
+            break;
+        default:
+            break;
+    }
+    return month_number;
+}
+
+int check_if_cache_entry_expire(char *u_r_l, struct tm * time_now){
+    int i =0 , return_value;
+    char updated_time[TIME_LENGTH];
+    for(i=0;i<10;i++)
+    {
+        if(strcmp(cache[i].u_r_l,u_r_l)==0)
+        {
+            break;
+        }
+    }
+    memset(updated_time,0, TIME_LENGTH);
+    sprintf(updated_time, "%s, %2d %s %4d %2d:%2d:%2d GMT", *day[time_now->tm_wday],time_now->tm_mday, month[time_now->tm_mon], time_now->tm_year+1900,time_now->tm_hour,time_now->tm_min,time_now->tm_sec);
+    return_value = time_comparison(cache[i].Exp, updated_time);
+    return (return_value<0)?-1:0;
+
+}
+int time_comparison(char *old_time, char *new_time){
+    int year_old, month_old, hour_old,minute_old,second_old,day_old;
+    int year_new, month_new, hour_new, minute_new, second_new, day_new;
+
+    char actual_old_month[4];
+    char actual_new_month[4];
+
+    memset(actual_new_month,0,4);
+    memset(actual_old_month,0,4);
+
+    sscanf(old_time + 5,"%d %3s %d %d:%d:%d ",&day_old, actual_old_month,&year_old,&hour_old,&minute_old,&second_old );
+    sscanf(new_time + 5,"%d %3s %d %d:%d:%d ",&day_new, actual_new_month,&year_new,&hour_new,&minute_new,&second_new );
+   
+    month_old = month_converter(actual_old_month);
+    month_new = month_converter(actual_new_month);
+
+    if(year_old < year_new) return -1;
+    if(year_old>year_new) return 1;
+    if(month_old<month_new) return -1;
+    if(month_old > month_new) return 1;
+    if (day_old < day_new) return -1;
+    if(day_old > day_new) return 1;
+    if(hour_old < hour_new) return -1;
+    if(hour_old>hour_new) return 1;
+    if(minute_old<minute_new) return -1;
+    if(minute_old > minute_new) return 1;
+    if (second_old < second_new) return -1;
+    if(second_old > second_new) return 1;
+
+    return 0;
+
+}
+
+void send_error_message (int status, int socket_file_descriptor){
+    char errorMessage[1024];
+    static char* bad_request = 
+    "===================================================="
+    "HTTP/1.0 400 Bad Request"
+    "====================================================";
+
+    static char *not_found = 
+    "===================================================="
+    "HTTP/1.0 404 not found"
+    "====================================================";
+
+    memset(errorMessage,0, 1024);
+    switch(status)
+    {
+        case 400:
+        sprintf(errorMessage,"%s", bad_request);
+        send(socket_file_descriptor, errorMessage, strlen(errorMessage),0);
+        break;
+
+        case 404:
+        sprintf(errorMessage,"%s",not_found);
+        send(socket_file_descriptor, errorMessage, strlen(errorMessage),0);
+        break;
+
+        default:
+        break;
+    }
 }
 #endif 
---
+++ Assignment_4_new/server.c
@@ -0,0 +1,369 @@
+/*********************************HTTP server implementation*************/
+//Author: Sanket Agarwal and Dhiraj Kudva
+//Organisation: Texas A&M university
+//Usage: used to html_request the html page from the proxt server. 
+
+// list of common files required for the functioning. 
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <signal.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include <stdint.h>
+#include <common_def.h>
+
+int main(int argc, char *argv[])
+{
+	char ack_request[1024];
+	time_t now;
+	struct tm *timenow;
+	struct addrinfo hint, *res;
+	int send_length;
+	FILE *fp;
+	int receive_length = -1;
+	int listener_filedescriptor, client_filedescriptor;
+	struct sockaddr_storage remote_addr;
+	struct sockaddr_in addr;
+	socklen_t sin_size;
+	fd_set set_1;
+	fd_set set_2;
+	int maximum_filedescriptor;
+	int socket_ITR;
+	char ac_Buffer[BUFFER_SIZE], ac_Host[NAME_LENGTH],ac_u_r_l[NAME_LENGTH], ac_Name[NAME_LENGTH];
+	int index_in_cache = -1;
+	int port_http = PORT_NO;
+	int proxy_filedescriptor;
+	int i, old_destination_entry;
+	FILE *read_filepointer;
+	char *expire = NULL;
+	char *pc_token = NULL;
+	int read_length =0;
+	char modified [100];
+	char modified_request[BUFFER_SIZE];
+
+	memset(cache,0,10*sizeof(_cache));
+	if(argc < 3)
+	{
+		printf("\n WRONG USAGE: ./<server> <host> <port> \n");
+		exit(-1);
+	}
+	memset(&remote_addr,0, sizeof remote_addr);
+	memset(&addr,0,sizeof addr);
+
+	port_http = atoi(argv[2]);
+	addr.sin_port = htons(port_http);
+	addr.sin_addr.s_addr = inet_addr(argv[1]);
+	addr.sin_family = AF_INET;
+
+	if((listener_filedescriptor = socket(AF_INET, SOCK_STREAM, 0)) <0)
+	{
+		system_error("ERROR: In creating socket");
+		exit(-1);
+	}
+	if(bind(listener_filedescriptor,(struct sockaddr*)&addr, sizeof(struct sockaddr)) < 0)
+	{
+		system_error("ERROR: In bind socket");
+		exit(-1);
+	}
+	if(listen(listener_filedescriptor,10) <0)
+	{
+		system_error("ERROR: In listen");
+		exit(-1);
+	}
+
+	FD_SET(listener_filedescriptor, &set_1);
+	maximum_filedescriptor= listener_filedescriptor;
+
+	memset(cache,0,10*sizeof(_cache));
+
+	printf("WAITING... for request1 \n");
+	sin_size = sizeof(remote_addr);
+
+	while(1)
+	{
+		set_2= set_1;
+		if(select(maximum_filedescriptor+1, &set_2, NULL, NULL, NULL) == -1){
+			system_error("ERROR: In select");
+			exit(-1);
+		}
+		for (socket_ITR = 0; socket_ITR <=maximum_filedescriptor; socket_ITR++)
+		{
+			if(FD_ISSET(socket_ITR, &set_2))
+			{
+				if(socket_ITR == listener_filedescriptor)
+				{
+					client_filedescriptor= accept(listener_filedescriptor,(struct sockaddr*)&remote_addr,&sin_size);
+					maximum_filedescriptor= client_filedescriptor>maximum_filedescriptor ? client_filedescriptor : maximum_filedescriptor;
+					FD_SET(client_filedescriptor, &set_1);
+					if(client_filedescriptor == -1)
+					{
+						system_error("ERROR: In accept");
+						continue;
+					}
+				}
+				else
+				{
+					memset(ac_Buffer, 0, BUFFER_SIZE);
+					memset(ac_Host, 0, NAME_LENGTH);
+					memset(ac_u_r_l, 0, NAME_LENGTH);
+					memset(ac_Name, 0, NAME_LENGTH);
+
+					client_filedescriptor= socket_ITR;
+					if(recv(client_filedescriptor, ac_Buffer, sizeof(ac_Buffer),0) < 0)
+					{
+						system_error("ERROR: In recv");
+						close(client_filedescriptor);
+						return 1;
+					}
+					printf("Message sent from CLIENT : \n%s \n", ac_Buffer);
+					if(parse_read_request(ac_Buffer, ac_Host, &port_http, ac_u_r_l, ac_Name)!=4)
+					{
+						send_error_message(400, client_filedescriptor);
+						close(client_filedescriptor);
+						return 1;
+					}
+					if((proxy_filedescriptor = socket(AF_INET, SOCK_STREAM, 0)) <0){
+						system_error("ERROR: In PROXYSOCKET ERROR");
+						close(client_filedescriptor);
+						return 1;
+					}
+
+					memset(&hint, 0, sizeof(hint));
+					hint.ai_family = AF_INET;
+					hint.ai_socktype = SOCK_STREAM;
+
+					if(getaddrinfo(ac_Host,"80", &hint, &res)!=0)
+					{
+						printf("ERROR: GETADDRINFO\n");
+						send_error_message(404, client_filedescriptor);
+						close(client_filedescriptor);
+						return 1;
+					}
+					if(connect(proxy_filedescriptor, res->ai_addr, res->ai_addrlen) < 0)
+					{
+						close(proxy_filedescriptor);
+						system_error("ERROR: IN connect");
+						send_error_message(404, client_filedescriptor);
+						close(client_filedescriptor);
+						return 1;
+					}
+					time(&now);
+					timenow = gmtime(&now);
+
+					index_in_cache = check_if_cache_entry_present(ac_u_r_l);
+					if(index_in_cache == -1)
+					{
+						//new entry in cache. 
+						//Request is sent to HTTP server
+						memset(ack_request, 0, 1024);
+						printf("NOT FOUND IN CACHE...\n Downloading from %s:%d\n",ac_Host, port_http );
+						printf("\n SENDING ... HTTP QUERY -> WEB SERVER");
+						sprintf(ack_request, "GET %s HTTP/1.0\r\nHost: %s\r\n\r\n", ac_Name, ac_Host);
+						puts(ack_request);
+						if((send_length = send(proxy_filedescriptor, ack_request, strlen(ack_request),0)) <0)
+						{
+							system_error("ERROR: in send request");
+							close(proxy_filedescriptor);
+							return 1;
+						}
+						printf("REQUEST SENT -> SERVER \n");
+						memset(ac_Buffer ,0, BUFFER_SIZE);
+						for (i = 0; i< 10; i++)
+						{
+							if(cache[i].Is_filled == 0)
+							{
+								old_destination_entry = i;
+								break;
+							}
+							else
+							{
+								if(time_comparison(cache[i].Last_Modfd, cache[old_destination_entry].Last_Modfd) <= 0)
+								{
+									old_destination_entry = i;
+								}
+
+							}
+						}
+						memset(&cache[old_destination_entry], 0, sizeof(_cache));
+						cache[old_destination_entry].Is_filled = 1;
+
+						pc_token= strtok(ac_Name,"/");
+
+						while(pc_token != NULL)
+						{
+							strcpy(cache[old_destination_entry].Fname, pc_token);
+							pc_token = strtok(NULL, "/");
+						}
+
+						memcpy(cache[old_destination_entry].u_r_l, ac_u_r_l, NAME_LENGTH);
+						sprintf(cache[old_destination_entry].Last_Modfd,"%s, %02d %s %d %02d:%02d:%02d GMT", day[timenow->tm_wday], timenow ->tm_mday, month[timenow ->tm_mon], timenow->tm_year+ 1990, timenow->tm_hour, timenow->tm_min, timenow->tm_sec  );
+
+						remove(cache[old_destination_entry].Fname);
+						fp = fopen(cache[old_destination_entry].Fname, "w");
+						if(fp == NULL)
+						{
+							printf("ERROR : Failed to create CACHE\n");
+							return 1;
+						}
+						while((receive_length = recv(proxy_filedescriptor, ac_Buffer, BUFFER_SIZE, 0)) > 0)
+						{
+							if(send(client_filedescriptor, ac_Buffer, receive_length,0) <0)
+							{
+								system_error("ERROR: Client send");
+								return 1;
+							}
+							fwrite(ac_Buffer, 1, receive_length, fp);
+							memset(ac_Buffer, 0, BUFFER_SIZE);
+						}
+						send(client_filedescriptor, ac_Buffer, 0, 0);
+						printf("SUCCESS: Received response from server\n");
+						printf("SENT FILE TO THE CLIENT \n ");
+
+						fclose(fp);
+
+						read_filepointer = fopen(cache[old_destination_entry].Fname, "r");
+						fread(ac_Buffer, 1, 2048, read_filepointer);
+						fclose(read_filepointer);
+
+						expire = strstr(ac_Buffer, "EXPIRES: ");
+						if(expire != NULL)
+						{
+							memcpy(cache[old_destination_entry].Exp, expire+9, 29);
+						}
+						else
+						{
+							sprintf(cache[old_destination_entry].Exp, "%s, %02d %s %d %02d:%02d:%02d GMT", day[timenow->tm_wday], timenow ->tm_mday, month[timenow ->tm_mon], timenow->tm_year+ 1990, timenow->tm_hour, (timenow->tm_min)+2, timenow->tm_sec );			
+						}
+					}
+					else
+					{
+						if(check_if_cache_entry_expire(ac_u_r_l, timenow) >=0)
+						{
+							printf("FILE FOUND: in cache AND NOT EXPIRED\n Sending file to client..\n");
+							sprintf(cache[index_in_cache].Last_Modfd,"%s, %02d %s %d %02d:%02d:%02d GMT", day[timenow->tm_wday], timenow ->tm_mday, month[timenow ->tm_mon], timenow->tm_year+ 1990, timenow->tm_hour, timenow->tm_min, timenow->tm_sec  );
+							read_filepointer = fopen(cache[index_in_cache].Fname, "r");
+							memset(ac_Buffer, 0, BUFFER_SIZE);
+							while((read_length= fread(ac_Buffer,1 , BUFFER_SIZE, read_filepointer)) > 0)
+							{
+								send(client_filedescriptor, ac_Buffer, read_length, 0);
+							}
+							printf("SUCCESS: SENT FILE TO CLIENT");
+							fclose(read_filepointer);
+						}
+						else
+						{
+							printf("FILE EXPIRED. REQUESTING UPDATED FILE (from SERVER) \n");
+							memset(modified, 0, 100);
+							sprintf(modified, "If-Modified-Since: %s\r\n\r\n", cache[index_in_cache].Last_Modfd);
+							memset(modified_request, 0, BUFFER_SIZE);
+							memcpy(modified_request, ac_Buffer, strlen(ac_Buffer)-2);
+
+							strcat(modified_request, modified);
+							printf("SENDING HTTP QUERY -> WEB SERVER\n");
+							printf("%s\n", modified_request);
+							send(proxy_filedescriptor, modified_request, strlen(modified_request),0);
+							memset(ac_Buffer,0, BUFFER_SIZE);
+							receive_length = recv(proxy_filedescriptor, ac_Buffer, BUFFER_SIZE, 0);
+							expire =  strstr(ac_Buffer, "Expires : ");
+							if(expire != NULL)
+							{
+								memcpy(cache[index_in_cache].Exp, expire+9, 29);
+							}
+							else
+							{
+								sprintf(cache[index_in_cache].Exp, "%s, %02d %s %d %02d:%02d:%02d GMT", day[timenow->tm_wday], timenow ->tm_mday, month[timenow ->tm_mon], timenow->tm_year+ 1990, timenow->tm_hour, timenow->tm_min, timenow->tm_sec );
+
+							}
+							if(receive_length > 0)
+							{
+								printf("HTTP RESPONSE: \n %s\n",ac_Buffer );
+								if((*(ac_Buffer + 9) == '3') && (*(ac_Buffer + 10) == '0') && (*(ac_Buffer + 11) == '4'))
+								{
+									printf("FILE UPTO DATE. sending file from cache\n");
+									read_filepointer = fopen(cache[index_in_cache].Fname, "r");
+									memset(ac_Buffer, 0, BUFFER_SIZE);
+									while((read_length = fread(ac_Buffer, 1, BUFFER_SIZE, read_filepointer)) >0)
+									{
+										send(client_filedescriptor, ac_Buffer, read_length, 0);
+									}
+									fclose(read_filepointer);
+								}	
+								else if ((*(ac_Buffer+9) == '4') && (*(ac_Buffer +10 ) =='0') &&(*(ac_Buffer+11) == '4'))
+								{
+									send_error_message(404, client_filedescriptor);
+								}
+								else if((*(ac_Buffer+9) =='2') && (*(ac_Buffer +10) == '0') && (*(ac_Buffer +11) == '0'))
+								{
+									printf("NEW FILE RECEIVED FROM SERVER.\n UPDATE CACHE AND SEND FILE TO CLIENT\n");
+									send(client_filedescriptor, ac_Buffer, receive_length, 0);
+									remove(cache[index_in_cache].Fname);
+
+									expire = NULL;
+									expire = strstr(ac_Buffer, "EXPIRES:");
+									if(expire != NULL)
+									{
+										memcpy(cache[index_in_cache].Exp, expire+9,29);
+									}
+									else
+									{
+										sprintf(cache[index_in_cache].Exp, "%s, %02d %s %d %02d:%02d:%02d GMT", day[timenow->tm_wday], timenow ->tm_mday, month[timenow ->tm_mon], timenow->tm_year+ 1990, timenow->tm_hour, timenow->tm_min, timenow->tm_sec );
+
+									}
+									sprintf(cache[index_in_cache].Last_Modfd, "%s, %02d %s %d %02d:%02d:%02d GMT", day[timenow->tm_wday], timenow ->tm_mday, month[timenow ->tm_mon], timenow->tm_year+ 1990, timenow->tm_hour, timenow->tm_min, timenow->tm_sec );
+									fp = fopen(cache[index_in_cache].Fname, "w");
+									fwrite(ac_Buffer, 1, receive_length, fp);
+
+									memset(ac_Buffer, 0, BUFFER_SIZE);
+									while((receive_length = recv(proxy_filedescriptor, ac_Buffer, BUFFER_SIZE, 0)) > 0)
+									{
+										send(client_filedescriptor, ac_Buffer, receive_length,0);
+										fwrite(ac_Buffer, 1, receive_length, fp);
+									}
+									fclose(fp);
+
+								}
+							}
+							else
+							{
+								system_error("ERROR: In receive");
+							}
+						}
+					}
+					FD_CLR(client_filedescriptor, &set_1);
+					close(client_filedescriptor);
+					close(proxy_filedescriptor);
+					printf("\n printing cache table \n");
+					for (i=0;i<10;i++)
+					{
+						if(cache[i].Is_filled)
+						{
+							printf("Cache Entry Number %d\n", i + 1 );
+							printf("URL              : %s\n",cache[i].u_r_l);
+							printf("Last Access Time : %s\n",cache[i].Last_Modfd);
+							printf("Expiry           : %s\n", cache[i].Exp );
+							printf("File Name        : %s\n", cache[i].Fname);
+						}
+					}
+				}
+			}
+		}
+
+	}
+}
